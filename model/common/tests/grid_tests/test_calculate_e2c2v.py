99fff449bf4efa535c0495e2ceb177fae5b9477e
mklein <realmichaelklein@gmail.com>
2025-03-05 10:38:20 +0100

    setup

--- model/common/tests/grid_tests/test_calculate_e2c2v.py
+++ model/common/tests/grid_tests/test_calculate_e2c2v.py
@@ -2,11 +2,19 @@ import numpy as np
 import pytest
 
 import icon4py.model.common.utils.data_allocation as data_alloc
-from icon4py.model.atmosphere.diffusion.stencils.calculate_e2c2v_simple import (
+
+from icon4py.model.common.grid.calculate_e2c2v_simple import (
     c2v_stecil,
     e2c_stencil,
     e2c2v_stencil,
 )
+
+from icon4py.model.common.grid.calculate_e2c2v_manualreduction import (
+    e2c2v_manualreduction_stencil
+)
+
+def are_arrays_equal(arr1, arr2):
+    return Counter(arr1) == Counter(arr2)
 
 def _c2v_stencil_numpy(vertex_input: np.ndarray) -> np.ndarray:
     return np.sum(vertex_input, axis=0)
@@ -17,6 +25,9 @@ def _e2c_stencil_numpy(cell_input: np.ndarray) -> np.ndarray:
 def _e2c2v_stencil_numpy(vertex_input: np.ndarray) -> np.ndarray:
     tmp = _c2v_stencil_numpy(vertex_input)
     return _e2c_stencil_numpy(tmp)
+
+def _e2c2v_manualreduction_stencil_numpy(vertex_input: np.ndarray) -> np.ndarray:
+    return np.array([])
 
 class TestCalculateE2C2V:
     
