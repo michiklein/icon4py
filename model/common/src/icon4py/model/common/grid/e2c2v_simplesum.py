42e3612e1b85a3393512b21eec078a58be1885b9
mklein <realmichaelklein@gmail.com>
2025-03-18 11:48:19 +0100

    Revert "trying to get a neighbor table going"
    This reverts commit dba79c436abb24604e22e5e0a8f35c82b6669021.

---
+++ model/common/src/icon4py/model/common/grid/e2c2v_simplesum.py
@@ -0,0 +1,26 @@
+from gt4py.next.ffront.decorator import field_operator, program
+
+from icon4py.model.common import field_type_aliases as fa
+from icon4py.model.common.dimension import C2V, E2C
+
+
+@field_operator
+def _E2C2V_simple_sum(
+    vertex_input: fa.VertexKField[float],
+) -> fa.EdgeKField[float]:
+    return (
+        vertex_input(C2V[0])(E2C[0])
+        + vertex_input(C2V[1])(E2C[0])
+        + vertex_input(C2V[0])(E2C[1])  # + vertex_input(C2V[1])(E2C[0]), replace for slightly smarter sum
+        + vertex_input(C2V[2])(E2C[0])
+        + vertex_input(C2V[1])(E2C[1])  # + vertex_input(C2V[2])(E2C[0]), replace for slightly smarter sum
+        + vertex_input(C2V[2])(E2C[1])
+    )
+
+
+@program
+def E2C2V_simple_sum(
+    vertex_input: fa.VertexKField[float],
+    edge_out: fa.EdgeKField[float],
+):
+    _E2C2V_simple_sum(vertex_input, out=edge_out)
